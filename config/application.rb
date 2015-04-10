f8376f37552122b999187d27d79521349cdd8775
brossetti1 <brossetti1@gmail>
2015-04-10 16:05:28 -0400

    adding rac cors config

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,13 @@ module InnoBe
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # rac-cors config
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :patch, :delete, :put, :options]
+      end
+    end
   end
 end
