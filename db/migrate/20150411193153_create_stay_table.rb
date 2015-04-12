065da9c925e4d326135e568e7c745597ff148014
brossetti1 <brossetti1@gmail>
2015-04-11 22:10:20 -0400

    updating routes

--- db/migrate/20150411193153_create_stay_table.rb
+++
@@ -1,7 +0,0 @@
-class CreateStayTable < ActiveRecord::Migration
-  def change
-    create_table :stay do |t|
-      t.integer :patient_id
-    end
-  end
-end
--- db/migrate/20150411195219_rename_stay_table.rb
+++
@@ -1,5 +0,0 @@
-class RenameStayTable < ActiveRecord::Migration
-  def change
-    rename_table :stay, :stays
-  end
-end
