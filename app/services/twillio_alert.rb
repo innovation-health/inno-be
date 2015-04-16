f2e94f3e9fb83f0cd77626cedf8dacd1aac89429
brossetti1 <brossetti1@gmail>
2015-04-16 16:29:55 -0400

    updating inno-be

--- app/services/twillio_alert.rb
+++ app/services/twillio_alert.rb
@@ -1,13 +1,16 @@
 class TwilioAlert
 
-  def initialize()
+  def initialize(staff_waiting, staff_visiting, patient)
+    @staff_waiting   ||= staff_waiting
+    @staff_visiting  ||= staff_visiting
+    @patient         ||= patient
   end
 
   def call
+    send_text_message
   end
 
   def send_text_message
-    number_to_send_to = params[:number_to_send_to]
  
     twilio_sid   = ENV['account_sid']
     twilio_token = ENV['auth_token']
@@ -17,8 +20,8 @@ class TwilioAlert
  
     @twilio_client.account.sms.messages.create(
       :from => "+1#{twilio_phone}",
-      :to => number_to_send_to,
-      :body => "This is an message. It gets sent to #{number_to_send_to}"
+      :to => @staff_waiting.cell,
+      :body => "#{@staff_visiting.name} has stopped by #{@patient.name}'s room"
     )
   end
 end
